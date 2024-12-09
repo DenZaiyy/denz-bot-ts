b02e6b7aae1100213b5993ab63d328928952618b
DenZaiyy <denzaiyy@outlook.fr>
2024-12-09 21:40:44 +0100

    feat: add utils for prisma

---
+++ src/utils/db.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function getAllGuilds() {
+    try {
+        const guilds = await prisma.guild.findMany();
+        console.log(guilds);
+        return guilds;
+    } catch (err) {
+        console.error("Error fetching guilds: ", err);
+    } finally {
+        prisma.$disconnect;
+    }
+}
+
+export async function getAllMemberFromGuild(guildId: string) {
+    try {
+        const membersFromGuild = await prisma.member.findMany({
+            where: {
+                guildId: guildId,
+            },
+        });
+        console.log(membersFromGuild);
+        return membersFromGuild;
+    } catch (error) {
+        console.error("Error fetching member from guild: ", error);
+    } finally {
+        prisma.$disconnect;
+    }
+}
+
+export async function getAllStreamLiveFromGuild(guildId: string) {
+    try {
+        const liveStreamFromGuild = await prisma.live.findMany({
+            where: {
+                guildId: guildId,
+            },
+        });
+        console.log(liveStreamFromGuild);
+        return liveStreamFromGuild;
+    } catch (error) {
+        console.error("Error fetching streaming live from guild: ", error);
+    } finally {
+        prisma.$disconnect;
+    }
+}
