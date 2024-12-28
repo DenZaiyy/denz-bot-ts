381ca27b32a817353b24178f64c1c3cc2af248ef
DenZaiyy <denzaiyy@outlook.fr>
2024-12-28 14:23:52 +0100

    feat: add clear command

---
+++ src/slashCommands/clear.ts
@@ -0,0 +1,82 @@
+import {
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+    TextBasedChannel,
+    TextChannel,
+} from "discord.js";
+import { SlashCommand } from "../types";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const command: SlashCommand = {
+    name: "clear",
+    data: new SlashCommandBuilder()
+        .setName("clear")
+        .setDescription("Supprimer des messages dans un canal.")
+        .addStringOption((option) => {
+            return option
+                .setName("amount")
+                .setDescription("Nombre de messages à supprimer.")
+                .addChoices(
+                    {
+                        name: "10",
+                        value: "10",
+                    },
+                    {
+                        name: "20",
+                        value: "20",
+                    },
+                    {
+                        name: "50",
+                        value: "50",
+                    },
+                    {
+                        name: "all",
+                        value: "all",
+                    }
+                )
+                .setRequired(true);
+        })
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    execute: async (interaction) => {
+        if (!interaction.guild) {
+            await interaction.reply({
+                content:
+                    "Cette commande doit être utilisée dans un serveur Discord.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        const guildId = interaction.guild.id;
+        const amount = interaction.options.get("amount")!.value!.toString();
+
+        const channel = interaction.channel as TextChannel;
+
+        if (!channel || !channel.bulkDelete) {
+            await interaction.reply({
+                content:
+                    "Cette commande ne peut être utilisée que dans un canal de texte.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        if (amount === "all") {
+            await channel.bulkDelete(await channel.messages.fetch());
+            await interaction.reply({
+                content: "Tous les messages ont été supprimés.",
+                ephemeral: true,
+            });
+        } else {
+            await channel.bulkDelete(
+                await channel.messages.fetch({ limit: parseInt(amount) })
+            );
+            await interaction.reply({
+                content: `Les ${amount} derniers messages ont été supprimés.`,
+                ephemeral: true,
+            });
+        }
+    },
+};
