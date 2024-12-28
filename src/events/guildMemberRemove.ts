719dca3482a219a0a60b774de1cfa72bed7418bf
DenZaiyy <denzaiyy@outlook.fr>
2024-12-28 14:24:12 +0100

    feat: add event for member and update add event

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -5,43 +5,12 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const event: BotEvent = {
-    name: Events.GuildMemberAdd,
+    name: Events.GuildMemberRemove,
     once: false,
     execute: async (member: GuildMember) => {
         const guild = member.guild;
 
-        const colors = [
-            Colors.Red,
-            Colors.Green,
-            Colors.Blue,
-            Colors.Yellow,
-            Colors.Purple,
-        ];
-
-        const embed = new EmbedBuilder()
-            .setColor(colors[Math.floor(Math.random() * colors.length)])
-            .setDescription(
-                `${member.user.username} nous a rejoint !\n\nBienvenue sur le serveur !`
-            )
-            .setThumbnail(guild.iconURL())
-            .setAuthor({
-                name: member.user.username,
-                iconURL: member.user.avatarURL() || undefined,
-            })
-            .setTimestamp();
-
         try {
-            // Ajouter le member à la base de données par rapport au serveur
-            await prisma.member.create({
-                data: {
-                    userId: member.id,
-                    userName: member.user.username,
-                    role: member.roles.highest.name,
-                    guildId: guild.id,
-                    joinedAt: new Date(),
-                },
-            });
-
             const guildData = await prisma.guild.findUnique({
                 where: {
                     guildId: guild.id,
@@ -51,18 +20,34 @@ const event: BotEvent = {
                 },
             });
 
+            const memberData = await prisma.member.findFirst({
+                where: {
+                    userId: member.id,
+                },
+            });
+
             if (guildData && guildData.welcomeChannel) {
                 const welcomeChannelId = guildData.welcomeChannel;
 
                 const channel = await guild.channels.fetch(welcomeChannelId);
 
                 if (channel && channel.isTextBased()) {
-                    await channel.send({ embeds: [embed] });
+                    await channel.send(
+                        `**${member.user.username}** a quitté le serveur !`
+                    );
                 } else {
                     console.warn(
                         `Le canal ${welcomeChannelId} pour le serveur ${guild.name} n'est pas un canal textuel ou n'existe pas.`
                     );
                 }
+            }
+
+            if (memberData) {
+                await prisma.member.delete({
+                    where: {
+                        id: memberData.id,
+                    },
+                });
             }
         } catch (error) {
             console.error(`❌ Error adding guild member: ${guild.name}`, error);
