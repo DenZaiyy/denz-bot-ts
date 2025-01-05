191748f622809e047aa530f64ac879f591fc2830
DenZaiyy <denzaiyy@outlook.fr>
2025-01-05 17:50:46 +0100

    fix: prettier slashcommand files

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -2,20 +2,18 @@ import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "../types";
 
 export const command: SlashCommand = {
-    name: "ping",
-    data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Reply with ping of the bot."),
-    execute: async (interaction) => {
-        await interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                    .setAuthor({ name: "DenZaiyy" })
-                    .setDescription(
-                        `🏓 Pong! 🏓\nPing: ${interaction.client.ws.ping}ms`
-                    )
-                    .setColor("#ff8e4d"),
-            ],
-        });
-    },
+  name: "ping",
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Reply with ping of the bot."),
+  execute: async (interaction) => {
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setAuthor({ name: "DenZaiyy" })
+          .setDescription(`🏓 Pong! 🏓\nPing: ${interaction.client.ws.ping}ms`)
+          .setColor("#ff8e4d"),
+      ],
+    });
+  },
 };
--- src/slashCommands/react.ts
+++ src/slashCommands/react.ts
@@ -2,16 +2,16 @@ import { Message, SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "../types";
 
 export const command: SlashCommand = {
-    name: "react",
-    data: new SlashCommandBuilder()
-        .setName("react")
-        .setDescription("Reply with reaction."),
-    execute: async (interaction) => {
-        const message: Message = await interaction.reply({
-            content: "React to this message!",
-            fetchReply: true,
-        });
+  name: "react",
+  data: new SlashCommandBuilder()
+    .setName("react")
+    .setDescription("Reply with reaction."),
+  execute: async (interaction) => {
+    const message: Message = await interaction.reply({
+      content: "React to this message!",
+      fetchReply: true,
+    });
 
-        await message.react("👍");
-    },
+    await message.react("👍");
+  },
 };
