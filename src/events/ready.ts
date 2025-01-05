31869ae44f5a79c4fd590dcbf156beaa827d67e0
DenZaiyy <denzaiyy@outlook.fr>
2025-01-05 17:49:13 +0100

    fix: prettier ready event file

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,11 +2,11 @@ import { Client, Events } from "discord.js";
 import { BotEvent } from "../types";
 
 const event: BotEvent = {
-    name: Events.ClientReady,
-    once: true,
-    execute: (client: Client) => {
-        console.log(`🤖 ${client.user?.username} is ready! 🤖`);
-    },
+  name: Events.ClientReady,
+  once: true,
+  execute: (client: Client) => {
+    console.log(`🤖 ${client.user?.username} is ready! 🤖`);
+  },
 };
 
 export default event;
