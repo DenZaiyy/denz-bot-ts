7a0cb24b893ec4ca0e115781c0b8df78c6cfa431
DenZaiyy <denzaiyy@outlook.fr>
2025-01-05 17:46:51 +0100

    add: ssl passphrase from env file

--- src/config.ts
+++ src/config.ts
@@ -2,35 +2,38 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 const {
-    DISCORD_TOKEN,
-    DISCORD_CLIENT_ID,
-    TWITCH_CLIENT_ID,
-    TWITCH_CLIENT_SECRET,
-    TWITCH_SECRET,
-    TWITCH_CHANNEL,
-    API_URL,
+  DISCORD_TOKEN,
+  DISCORD_CLIENT_ID,
+  TWITCH_CLIENT_ID,
+  TWITCH_CLIENT_SECRET,
+  TWITCH_SECRET,
+  TWITCH_CHANNEL,
+  API_URL,
+  SSL_PASSPHRASE,
 } = process.env;
 
 if (
-    !DISCORD_TOKEN ||
-    !DISCORD_CLIENT_ID ||
-    !TWITCH_CLIENT_ID ||
-    !TWITCH_CLIENT_SECRET ||
-    !TWITCH_SECRET ||
-    !TWITCH_CHANNEL ||
-    !API_URL
+  !DISCORD_TOKEN ||
+  !DISCORD_CLIENT_ID ||
+  !TWITCH_CLIENT_ID ||
+  !TWITCH_CLIENT_SECRET ||
+  !TWITCH_SECRET ||
+  !TWITCH_CHANNEL ||
+  !API_URL ||
+  !SSL_PASSPHRASE
 ) {
-    throw new Error(
-        "Missing environement variables. Please check your .env file."
-    );
+  throw new Error(
+    "Missing environement variables. Please check your .env file.",
+  );
 }
 
 export const config = {
-    DISCORD_TOKEN,
-    DISCORD_CLIENT_ID,
-    TWITCH_CLIENT_ID,
-    TWITCH_CLIENT_SECRET,
-    TWITCH_SECRET,
-    TWITCH_CHANNEL,
-    API_URL,
+  DISCORD_TOKEN,
+  DISCORD_CLIENT_ID,
+  TWITCH_CLIENT_ID,
+  TWITCH_CLIENT_SECRET,
+  TWITCH_SECRET,
+  TWITCH_CHANNEL,
+  API_URL,
+  SSL_PASSPHRASE,
 };
