ceb5c4d62fd350c42ee85b7dce0fa84b187f6f2a
DenZaiyy <denzaiyy@outlook.fr>
2025-01-05 17:46:36 +0100

    fix: prettier

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,68 +1,66 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "./variables";
 
 export async function getAllGuilds() {
-    try {
-        const guilds = await prisma.guild.findMany();
-        console.log(guilds);
-        return guilds;
-    } catch (err) {
-        console.error("Error fetching guilds: ", err);
-    } finally {
-        prisma.$disconnect;
-    }
+  try {
+    const guilds = await prisma.guild.findMany();
+    console.log(guilds);
+    return guilds;
+  } catch (err) {
+    console.error("Error fetching guilds: ", err);
+  } finally {
+    prisma.$disconnect;
+  }
 }
 
 export async function getAllMemberFromGuild(guildId: string) {
-    try {
-        const membersFromGuild = await prisma.member.findMany({
-            where: {
-                guildId: guildId,
-            },
-        });
-        console.log(membersFromGuild);
-        return membersFromGuild;
-    } catch (error) {
-        console.error("Error fetching member from guild: ", error);
-    } finally {
-        prisma.$disconnect;
-    }
+  try {
+    const membersFromGuild = await prisma.member.findMany({
+      where: {
+        guildId: guildId,
+      },
+    });
+    console.log(membersFromGuild);
+    return membersFromGuild;
+  } catch (error) {
+    console.error("Error fetching member from guild: ", error);
+  } finally {
+    prisma.$disconnect;
+  }
 }
 
 export async function getAllStreamLiveFromGuild(guildId: string) {
-    try {
-        const liveStreamFromGuild = await prisma.live.findMany({
-            where: {
-                guildId: guildId,
-            },
-        });
-        console.log(liveStreamFromGuild);
-        return liveStreamFromGuild;
-    } catch (error) {
-        console.error("Error fetching streaming live from guild: ", error);
-    } finally {
-        prisma.$disconnect;
-    }
+  try {
+    const liveStreamFromGuild = await prisma.live.findMany({
+      where: {
+        guildId: guildId,
+      },
+    });
+    console.log(liveStreamFromGuild);
+    return liveStreamFromGuild;
+  } catch (error) {
+    console.error("Error fetching streaming live from guild: ", error);
+  } finally {
+    prisma.$disconnect;
+  }
 }
 
 export async function getWelcomeChannelFromGuild(guildId: string) {
-    try {
-        const guild = await prisma.guild.findUnique({
-            where: {
-                guildId: guildId,
-            },
-        });
+  try {
+    const guild = await prisma.guild.findUnique({
+      where: {
+        guildId: guildId,
+      },
+    });
 
-        if (!guild) {
-            console.error("Guild not found");
-            return null;
-        }
-
-        return guild.welcomeChannel;
-    } catch (error) {
-        console.error("Error fetching welcome channel from guild: ", error);
-    } finally {
-        prisma.$disconnect;
+    if (!guild) {
+      console.error("Guild not found");
+      return null;
     }
+
+    return guild.welcomeChannel;
+  } catch (error) {
+    console.error("Error fetching welcome channel from guild: ", error);
+  } finally {
+    prisma.$disconnect;
+  }
 }
--- src/utils/twitch.ts
+++ src/utils/twitch.ts
@@ -1,15 +1,7 @@
-import TwitchAPI from "../twitchAPI";
 import { config } from "../config";
-import { PrismaClient } from "@prisma/client";
 import { DMChannel, NewsChannel, TextChannel } from "discord.js";
 import { client } from "../bot";
-
-const prisma = new PrismaClient();
-
-const twitchAPI = new TwitchAPI(
-    config.TWITCH_CLIENT_ID,
-    config.TWITCH_CLIENT_SECRET
-);
+import { prisma, twitchAPI } from "./variables";
 
 let notifiedChannels: { [key: string]: { [guildId: string]: boolean } } = {};
 let userName = "";
@@ -18,142 +10,140 @@ let streamTitle = "";
 let gameLink = "";
 
 export async function isStreamLive(
-    oauthToken: string,
-    channel: string
+  oauthToken: string,
+  channel: string,
 ): Promise<boolean> {
-    const url = `https://api.twitch.tv/helix/streams?user_login=${channel}`;
-    try {
-        const response = await fetch(url, {
-            headers: {
-                "Client-Id": config.TWITCH_CLIENT_ID,
-                Authorization: `Bearer ${oauthToken}`,
-            },
-        });
+  const url = `https://api.twitch.tv/helix/streams?user_login=${channel}`;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Client-Id": config.TWITCH_CLIENT_ID,
+        Authorization: `Bearer ${oauthToken}`,
+      },
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (data.data.length > 0) {
-            userName = data.data[0]?.user_name;
-            category = data.data[0]?.game_name;
-            streamTitle = data.data[0]?.title;
-            gameLink = await getGameLink(data.data[0]?.game_id);
-        }
-
-        return data.data.length > 0;
-    } catch (error) {
-        console.error(`Error checking stream status for ${channel}:`, error);
-        return false;
+    if (data.data.length > 0) {
+      userName = data.data[0]?.user_name;
+      category = data.data[0]?.game_name;
+      streamTitle = data.data[0]?.title;
+      gameLink = await getGameLink(data.data[0]?.game_id);
     }
+
+    return data.data.length > 0;
+  } catch (error) {
+    console.error(`Error checking stream status for ${channel}:`, error);
+    return false;
+  }
 }
 
 async function getGameLink(gameId: string): Promise<string> {
-    const url = `https://api.twitch.tv/helix/games?id=${gameId}`;
-    try {
-        const response = await fetch(url, {
-            headers: {
-                "Client-Id": config.TWITCH_CLIENT_ID,
-                Authorization: `Bearer ${await twitchAPI.getTwitchOAuthToken()}`,
-            },
-        });
-        const data = await response.json();
+  const url = `https://api.twitch.tv/helix/games?id=${gameId}`;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Client-Id": config.TWITCH_CLIENT_ID,
+        Authorization: `Bearer ${await twitchAPI.getTwitchOAuthToken()}`,
+      },
+    });
+    const data = await response.json();
 
-        const gameName = data.data[0]?.name;
-        const formattedGameName = gameName.replace(/ /g, "-");
+    const gameName = data.data[0]?.name;
+    const formattedGameName = gameName.replace(/ /g, "-");
 
-        if (data.data.length === 0) {
-            return "";
-        }
-
-        return `https://www.twitch.tv/directory/game/${formattedGameName.toLowerCase()}`;
-    } catch (error) {
-        console.error(`Error getting game link for game id ${gameId}:`, error);
-        return "";
+    if (data.data.length === 0) {
+      return "";
     }
+
+    return `https://www.twitch.tv/directory/game/${formattedGameName.toLowerCase()}`;
+  } catch (error) {
+    console.error(`Error getting game link for game id ${gameId}:`, error);
+    return "";
+  }
 }
 
 export async function sendStreamNotification(
-    streamerName: string,
-    guildId: string
+  streamerName: string,
+  guildId: string,
 ) {
-    try {
-        const liveChannel = await prisma.guild.findUnique({
-            select: {
-                annoucementChannel: true,
-                name: true,
-            },
-            where: {
-                guildId: guildId,
-            },
-        });
+  try {
+    const liveChannel = await prisma.guild.findUnique({
+      select: {
+        annoucementChannel: true,
+        name: true,
+      },
+      where: {
+        guildId: guildId,
+      },
+    });
 
-        if (!liveChannel) {
-            console.log(`Guild not found: ${guildId}`);
-            return;
-        }
-
-        if (!liveChannel.annoucementChannel) {
-            console.log(
-                `[${liveChannel.name}] No annoucement channel found for guild.`
-            );
-            return;
-        }
-
-        const channel = await client.channels.fetch(
-            liveChannel.annoucementChannel
-        );
-
-        if (
-            channel instanceof TextChannel ||
-            channel instanceof NewsChannel ||
-            channel instanceof DMChannel
-        ) {
-            await channel.send(
-                `🚨 @everyone **${userName}** viens de lancer son live sur Twitch!\n**Titre:** ${streamTitle}\n**Catégorie:** ${category}\nRejoint le ici: https://www.twitch.tv/${streamerName} 🚨`
-            );
-        } else {
-            console.log(
-                "Could not find the Discord channel or it is not a text channel."
-            );
-        }
-    } catch (error) {
-        console.error("Error sending notification to Discord channel:", error);
+    if (!liveChannel) {
+      console.log(`Guild not found: ${guildId}`);
+      return;
     }
+
+    if (!liveChannel.annoucementChannel) {
+      console.log(
+        `[${liveChannel.name}] No annoucement channel found for guild.`,
+      );
+      return;
+    }
+
+    const channel = await client.channels.fetch(liveChannel.annoucementChannel);
+
+    if (
+      channel instanceof TextChannel ||
+      channel instanceof NewsChannel ||
+      channel instanceof DMChannel
+    ) {
+      await channel.send(
+        `🚨 @everyone **${userName}** viens de lancer son live sur Twitch!\n**Titre:** ${streamTitle}\n**Catégorie:** ${category}\nRejoint le ici: https://www.twitch.tv/${streamerName} 🚨`,
+      );
+    } else {
+      console.log(
+        "Could not find the Discord channel or it is not a text channel.",
+      );
+    }
+  } catch (error) {
+    console.error("Error sending notification to Discord channel:", error);
+  }
 }
 
 export async function checkStreamStatus() {
-    const oauthToken = await twitchAPI.getTwitchOAuthToken();
-    if (!oauthToken) {
-        console.log("Twitch OAuth token not found.");
-        return;
+  const oauthToken = await twitchAPI.getTwitchOAuthToken();
+  if (!oauthToken) {
+    console.log("Twitch OAuth token not found.");
+    return;
+  }
+
+  const channels = await prisma.live.findMany({
+    select: {
+      channel: true,
+      guildId: true,
+    },
+    where: {
+      plateforme: "twitch",
+    },
+  });
+
+  for (const channel of channels) {
+    const isLive = await isStreamLive(oauthToken, channel.channel);
+
+    if (!notifiedChannels[channel.channel]) {
+      notifiedChannels[channel.channel] = {};
     }
 
-    const channels = await prisma.live.findMany({
-        select: {
-            channel: true,
-            guildId: true,
-        },
-        where: {
-            plateforme: "twitch",
-        },
-    });
+    const hasNotified = notifiedChannels[channel.channel][channel.guildId!];
 
-    for (const channel of channels) {
-        const isLive = await isStreamLive(oauthToken, channel.channel);
-
-        if (!notifiedChannels[channel.channel]) {
-            notifiedChannels[channel.channel] = {};
-        }
-
-        const hasNotified = notifiedChannels[channel.channel][channel.guildId!];
-
-        if (isLive && !hasNotified) {
-            await sendStreamNotification(channel.channel, channel.guildId!);
-            notifiedChannels[channel.channel][channel.guildId!] = true;
-        } else if (!isLive && hasNotified) {
-            notifiedChannels[channel.channel][channel.guildId!] = false;
-            // console.log(
-            //     `${channel.channel} is offline`
-            // );
-        }
+    if (isLive && !hasNotified) {
+      await sendStreamNotification(channel.channel, channel.guildId!);
+      notifiedChannels[channel.channel][channel.guildId!] = true;
+    } else if (!isLive && hasNotified) {
+      notifiedChannels[channel.channel][channel.guildId!] = false;
+      // console.log(
+      //     `${channel.channel} is offline`
+      // );
     }
+  }
 }
