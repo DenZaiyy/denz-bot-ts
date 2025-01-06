e266faf15a49b2e9abe483f3ade4d0f5bdc93125
DenZaiyy <denzaiyy@outlook.fr>
2025-01-06 16:20:54 +0100

    feat: add guildUpdate event

---
+++ src/events/guildUpdate.ts
@@ -0,0 +1,49 @@
+import { Events, ChannelType, GuildChannel } from "discord.js"
+import { BotEvent } from "../types"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const event: BotEvent = {
+	name: Events.GuildUpdate,
+	once: true,
+	execute: async (oldGuild, newGuild) => {
+		try {
+			// check if oldGuild exists in database
+			const old = await prisma.guild.findUnique({
+				where: {
+					guildId: oldGuild.id,
+				},
+			})
+
+			if (old) {
+				const oldName = old.name
+
+				await prisma.guild.update({
+					where: {
+						guildId: newGuild.id,
+					},
+					data: {
+						name: newGuild.name,
+					},
+				})
+
+				console.log(`✅ Guild updated: ${oldName} | ${newGuild.name}`)
+			} else {
+				await prisma.guild.create({
+					data: {
+						guildId: newGuild.id,
+						name: newGuild.name,
+					},
+				})
+			}
+		} catch (error) {
+			console.error(`❌ Error updating guild: ${oldGuild.name}`, error)
+		} finally {
+			// Toujours déconnecter Prisma
+			await prisma.$disconnect()
+		}
+	},
+}
+
+export default event
